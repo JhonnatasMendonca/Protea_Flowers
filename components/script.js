65c761908b7970a2554285364c32ac79212b310a
Keylane Cardoso <keylanecardoso28@gmail.com>
2023-06-13 23:58:04 -0300

    Mudanças no modo noturno
    Adição de imagens brancas na pasta images, adição de classes e ids no código js na parte do modo noturno. Todas as classes e ids das paginas já foram adicionados, falta só alguns detalhes do modal.

--- components/script.js
+++ components/script.js
@@ -79,17 +79,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
         $('#icon_lua').css("display", "none");
         $('body, footer, header, .principal-box, .comentarios, .main').css("background-color", "black"); 
-        $('a, li, span, h1, h2, h3, h4, p, label').css("color", "white");
+        $('a, li, span, h1, h2, h3, h4, h5, p, label').css("color", "white");
         $('#icon_sol').css("display", "block");
 
-        $('.box-demonstracao, .add_produto, .box-img, .box-container, .flor-parecida, .info, .box-centro-dados').css("background-color", "#0F270F");
+        $('.box-demonstracao, .add_produto, .box-img, .box-container, .flor-parecida, .info, .box-centro-dados, #perfil, #meus-pedidos, #favoritos, #vistos-recentemente, #nav-bottom, #produtos-catalogados, #minhas-vendas, #produtos-estoque, .box-modal').css("background-color", "#0F270F");
 
         // $('.add_produto').css("background-color", "black");
-        $('.botao-claro').css("background-color", "#859479");
+        $('.botao-claro, #finalizarCompra').css("background-color", "#859479");
         $('.card-form-vendedor').css("background-color", "#3D0E0A");
 
-        $('.img-lixo, #icon-face, #icon-insta, #icon-whatsapp, #icon-pinterest, #icon-tw, #icon-youtube').css("display", "none");
-        $('.img-lixo-branca, #icon-face-branca, #icon-insta-branca, #icon-whatsapp-branca, #icon-pinterest-branca, #icon-tw-branca, #icon-youtube-branca').css("display", "block");
+        $('.icon-normal').css("display", "none");
+        $('.icon-branco').css("display", "block");
         
 
 
@@ -111,21 +111,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
         $('.breadcrumbs span').css("color", "#859479");
-        $('a, li, span, h1, h2, h3, h4, p, label').css("color", "#0F270F");
+        $('a, li, span, h1, h2, h3, h4, h5, p, label').css("color", "#0F270F");
         $('.header-bottom .buttons a').css("color", "#efefee");
         $('.box-demonstracao, .box-img, .box-container').css("background-color", "#D9D9D9");
         $('box-opcoes li').css("color", "#0F270F"); 
         $('.texto-branco').css("color", "white"); 
         $('.breadcrumbs-final').css("color", "#859479"); 
         $('.page-atual').css("color", "#0F270F");
-        $('.add_produto, .card-form-vendedor, .box-centro-dados').css("background-color", "#FFFAF7");
-        $('.botao-claro').css("background-color", "#0F270F");
+        $('.box-modal').css("background-color", "#D9D9D9");
+        $('.add_produto, .card-form-vendedor, .box-centro-dados, #perfil, #meus-pedidos, #favoritos, #vistos-recentemente, #nav-bottom, #produtos-catalogados, #minhas-vendas, #produtos-estoque').css("background-color", "#FFFAF7");
+        $('.botao-claro, #finalizarCompra').css("background-color", "#0F270F");
         $('.principal-box, .comentarios').css("background-color", "#ffffff");
         $('.flor-parecida').css("background-color", "#DBD8CF");
         $('.info').css("background-color", "#DBE8D1");
 
-        $('.img-lixo, #icon-face, #icon-insta, #icon-whatsapp, #icon-pinterest, #icon-tw, #icon-youtube').css("display", "block");
-        $('.img-lixo-branca, #icon-face-branca, #icon-insta-branca, #icon-whatsapp-branca, #icon-pinterest-branca, #icon-tw-branca, #icon-youtube-branca').css("display", "none");
+        $('.icon-normal').css("display", "block");
+        $('.icon-branco').css("display", "none");
 
 
        
